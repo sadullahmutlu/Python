4d80af1f5821860a416abfbf8312de6d5cc12fd1
Sadullahmutlu <36160436+sadullahmutlu@users.noreply.github.com>
2021-02-15 12:46:57 +0300

    lambda intersaction example

--- Python_Example/lambda_example_2.py
+++ Python_Example/lambda_example_2.py
@@ -1,6 +1,6 @@
 
-liste_1 = [1, 2, 3, 5, 7, 8, 9, 10]
-liste_2= [1, 2, 4, 8, 9]
+liste_1 = [1, 2, 3, 5, 8, 10, 14, 18]
+liste_2= [4, 8, 9,20,12,18,14]
 
 #iki listenin elemanlarının keşimi
 sonuc = list(filter(lambda x: x in liste_1, liste_2)) 
